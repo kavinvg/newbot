1d572584018b5d006c2f743d90d6665ca4cb782d
kavinvg <128715421+kavinvg@users.noreply.github.com>
2023-05-01 20:45:22 +0530

    Add files via upload

--- exp2.c
+++ exp2.c
@@ -27,3 +27,32 @@ int main()
 the parent id is 3085
 the child id is 3086
 
+fullcode:
+#include<stdio.h>
+#include<sys/types.h>
+ #include<unistd.h>
+int fork_func()
+{
+fork();
+printf("The fork system is used \n");
+pid_t parent_id, child_id;
+parent_id=getppid( );
+printf("THE PARENT ID IS %d\n",parent_id );
+child_id=getpid();
+printf("THE CHILD ID IS %d\n",child_id);
+}
+
+int pid_func()
+{
+    pid_t parent_id,child_id;
+    parent_id = getppid();
+    printf("the parent id is %d\n",parent_id);
+    child_id = getpid();
+    printf("the child id is %d\n",child_id);
+}
+
+int main()
+{
+fork_func();
+pid_func();
+}
